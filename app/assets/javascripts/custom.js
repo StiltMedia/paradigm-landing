3ec63213780549540d7d348087fa63c221b42ddc
Mark Natera <mark@truthkingdom.com>
2015-02-13 00:39:17 -0500

    About page complete

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,19 @@
+// Nav Menu
+var menuRight = document.getElementById( 'cbp-spmenu-s2' ),
+  showRightPush = document.getElementById( 'showRightPush' ),
+  body = document.body;
+
+showRightPush.onclick = function() {
+  classie.toggle( this, 'active' );
+  classie.toggle( body, 'cbp-spmenu-push-toleft' );
+  classie.toggle( menuRight, 'cbp-spmenu-open' );
+};
+
+// Nav Toggle Bkg
+$( "#showRightPush" ).click(function() {
+$( ".menu-overlay" ).toggle();
+});
+
 
 // Scroll Function
 $(function() {
