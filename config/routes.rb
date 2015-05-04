c8a309634849cd5220f74f3ca7634590ee1aa8c6
Mark Natera <marknatera@gmail.com>
2015-05-04 18:16:32 -0400

    Removed clients sections

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'tags/:tag', to: 'clients#index', as: :tag
-  resources :clients
+
+  # get 'tags/:tag', to: 'clients#index', as: :tag
+  # resources :clients
 
 
   root 'pages#home'
